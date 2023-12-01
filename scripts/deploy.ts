75ad1e87c3a41e59395d8c6c999db03b58590ac7
Allen <haolong.zhang@mail.utoronto.ca>
2023-11-30 19:05:42 -0500

    bls contracts deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,59 @@
+import { writeFileSync } from "fs";
+import { createHash } from 'crypto';
+
 import { ethers } from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+import { DeterministicDeployer } from '@account-abstraction/sdk'
+import { EntryPoint__factory } from '@account-abstraction/contracts'
+import { aggregate, BlsSignerFactory, BlsVerifier } from '@thehubbleproject/bls/dist/signer'
+import { arrayify, defaultAbiCoder, hexConcat } from 'ethers/lib/utils'
+import {
+    BLSOpen__factory,
+    BLSSignatureAggregator__factory,
+    BLSAccount,
+    BLSAccount__factory,
+    BLSAccountFactory,
+    BLSAccountFactory__factory,
 
-  const lockedAmount = ethers.parseEther("0.001");
+} from '../typechain-types'
+import {
+    BrokenBLSAccountFactory__factory,
+    EntryPoint
+} from '@account-abstraction/contracts/types'
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+import * as SimpleAccountFactory from '@account-abstraction/contracts/artifacts/SimpleAccountFactory.json';
+import * as SimpleAccount from '@account-abstraction/contracts/artifacts/SimpleAccount.json';
+import { keccak256 } from 'ethereumjs-util'
+import { hashToPoint } from '@thehubbleproject/bls/dist/mcl'
+import { BigNumber, Signer } from 'ethers'
+import { BytesLike, hexValue } from '@ethersproject/bytes'
+import config from './config.json';
+import * as BLSSignatureAggregator from '../artifacts/contracts/bls/BLSSignatureAggregator.sol/BLSSignatureAggregator.json';
+export const HARDHAT_CHAIN = 31337;
+export const LOCAL_CHAIN = 1337;
 
-  await lock.waitForDeployment();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+
+
+async function deployBlsSignatureAggregator() {
+    const etherSigner = await ethers.provider.getSigner();
+    const entrypoint = config[11155111].entrypoint.address;
+    const BLSOpenLib = await new BLSOpen__factory(etherSigner).deploy()
+    const blsAgg = await new BLSSignatureAggregator__factory({
+        'contracts/bls/lib/BLSOpen.sol:BLSOpen': BLSOpenLib.address
+    }, ethers.provider.getSigner()).deploy(entrypoint)
+    console.log("Aggregator deployed at ", blsAgg.address);
+    const accountFactory = await new BLSAccountFactory__factory(etherSigner).deploy(entrypoint, blsAgg.address);
+    console.log("AccountFactory deployed at ", accountFactory.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function deployBlsAccount(blsSigner: any): Promise<BLSAccount> {
+    const etherSigner = await ethers.provider.getSigner();
+
+    const factory = BLSAccountFactory__factory.connect(config[11155111].factory.address, etherSigner)
+    const addr = await factory.callStatic.createAccount(0, blsSigner.pubkey)
+    await factory.createAccount(0, blsSigner.pubkey)
+    return BLSAccount__factory.connect(addr, etherSigner)
+}
+
+deployBlsSignatureAggregator();
